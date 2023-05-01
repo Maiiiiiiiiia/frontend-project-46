c25163606da81d77fb0e693529fa02d6a97eb1cb
maiiiiiiiiia <hi.maiakulieva@gmail.com>
2023-05-01 10:30:39 +0400

    second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,8 +4,11 @@ const { Command } = require('commander');
 const program = new Command();
 
 program
+  .version('0.1.0')
   .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.8.0');
+  .description('Compares two configuration files and shows a difference.');
+
+program
+  .option('-f, --format <type>', 'output format' )
 
 program.parse();
