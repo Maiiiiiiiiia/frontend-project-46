622b08f30c714bf3e7e70797b7df9e41185f2fa3
maiiiiiiiiia <hi.maiakulieva@gmail.com>
2023-05-01 13:22:24 +0400

    add Makefile, fix gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
-const { Command } = require('commander');
+import { Command } from 'commander';
 const program = new Command();
 
 program
-  .version('0.1.0')
   .name('gendiff')
-  .description('Compares two configuration files and shows a difference.');
-
-program
-  .option('-f, --format <type>', 'output format' )
+  .version('0.1.0')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>');
 
 program.parse();
