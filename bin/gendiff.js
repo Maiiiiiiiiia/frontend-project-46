d2d7b833ed71104a444884ca9a14fa4dec46abd0
maiiiiiiiiia <hi.maiakulieva@gmail.com>
2023-05-02 22:14:31 +0400

    step3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
+
 const program = new Command();
 
 program
@@ -8,7 +10,10 @@ program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format')
-  .argument('<filepath1>')
-  .argument('<filepath2>');
+  .argument('<filepath1>', 'path to file1')
+  .argument('<filepath2>', 'path to file2')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,36 @@
+import { readFileSync } from 'fs';
+import _ from 'lodash';
+import path from 'path';
+import { cwd } from 'node:process';
+
+const getDataParse = (pathFile) => {
+  const data = readFileSync(pathFile, 'utf-8');
+  return JSON.parse(data);
+};
+
+const genDiff = (filepath1, filepath2) => {
+  const resolvedFilepath1 = path.resolve(cwd(), filepath1);
+  const resolvedFilepath2 = path.resolve(cwd(), filepath2);
+
+  const data1 = getDataParse(resolvedFilepath1);
+  const data2 = getDataParse(resolvedFilepath2);
+
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const keys = _.sortBy(_.union(keys1, keys2));
+
+  const result = keys.map((key) => {
+    if (!_.has(data1, key)) {
+      return `+ ${key}: ${data2[key]}`;
+    }
+    if (!_.has(data2, key)) {
+      return `- ${key}: ${data1[key]}`;
+    }
+    if (_.isEqual(data1[key], data2[key])) {
+      return `  ${key}: ${data1[key]}`;
+    }
+    return [[`- ${key}: ${data1[key]}`], [`+ ${key}: ${data2[key]}`]];
+  });
+  return _.flatten(['{', ...result, '}']).join('\n');
+};
+export default genDiff;
